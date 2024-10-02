6bef2e2209f5d180950e154711a0cb4efec10d0b
Althea <altheacheng92@gmail.com>
2024-10-01 23:54:31 -1200

    Updated Avengers

--- Avengers.java
+++ Avengers.java
@@ -1,2 +1,19 @@
 public class Avengers {
+    //attributes
+    String name;
+    int power;
+
+    public void Punch() {
+        System.out.println("I can do this all day.");
+    }
+
+
+        public static void main(String[] args) {
+            Avengers hero1 = new Avengers();
+            hero1.name = "Captain America";
+            System.out.println(hero1.name);
+
+            hero1.Punch();
+
+    }
 }
